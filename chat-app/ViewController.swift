bfb618b8ef3e8bce3c5227aa664fb3a1b94654f9
DungHM <dunghm@rikkeisoft.com>
2024-12-30 09:27:00 +0700

    Initial Commit

---
+++ chat-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chat-app
+//
+//  Created by DungHM on 30/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
